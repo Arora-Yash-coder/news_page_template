eb5bf14a04b25af1ebf5225e1391ff1cf70da173
Yash Arora <yasharora9416@gmail.com>
2022-04-03 12:50:58 +0530

    Chore: Cleaned Up Project

---
+++ lib/newspage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyNewsPage extends StatelessWidget {
+  const MyNewsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
